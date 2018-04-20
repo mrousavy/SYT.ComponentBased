5da66ee46f0be7753a0fbca26fe1111efdc8013b
Marc Rousavy <marcrousavy@hotmail.com>
2018-04-20 11:04:17 +0200

    Add EMFactory, log4j properties and pom.xml

---
+++ Westbahnhof/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import BusinessObjects.Bahnhof;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        // TODO: DAL
+        logger.info("Starting EntityManagerFactory..");
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("westbahn");
+
+        EntityManager entitymanager = emfactory.createEntityManager();
+        entitymanager.getTransaction().begin();
+
+        Bahnhof bahnhof = new Bahnhof();
+
+        entitymanager.persist(bahnhof);
+        entitymanager.getTransaction().commit();
+
+        entitymanager.close();
+        emfactory.close();
+        logger.info("Finished!");
+    }
+}
